976026a1f9cc10c96209cf0f9c3781bf90f9c028
Dan Pock <self@danpock.me>
2025-02-10 18:19:54 -0500

    add logrus

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/mallardduck/ob-charts-tool/cmd"
+import (
+	"os"
+
+	"github.com/mallardduck/ob-charts-tool/cmd"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
+
 	cmd.Execute()
 }
