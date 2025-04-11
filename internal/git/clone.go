eb99c0305ae3a7442e2e25c586e68e6825a88787
Dan Pock <self@danpock.me>
2025-04-11 18:03:58 -0400

    Correct bug pulling specific commit hash

--- internal/git/clone.go
+++ internal/git/clone.go
@@ -1,7 +1,10 @@
 package git
 
 import (
+	"errors"
+	"fmt"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/rancher/ob-charts-tool/internal/git/cache"
 )
@@ -14,15 +17,55 @@ type RepoConfigParams struct {
 }
 
 func ShallowCloneRepository(repoConfig RepoConfigParams, destinationDir string) (plumbing.Hash, error) {
-	repoClone, err := git.PlainClone(destinationDir, false, &git.CloneOptions{
+	cloneOptions := git.CloneOptions{
 		URL:               repoConfig.URL,
 		Depth:             1,
 		RecurseSubmodules: git.NoRecurseSubmodules,
 		Progress:          nil,
-	})
+	}
+
+	// If a branch is provided, set it
+	if repoConfig.Branch != "" {
+		cloneOptions.ReferenceName = plumbing.NewBranchReferenceName(repoConfig.Branch)
+		cloneOptions.SingleBranch = true
+	}
+
+	repoClone, err := git.PlainClone(destinationDir, false, &cloneOptions)
 	if err != nil {
 		return plumbing.ZeroHash, err
 	}
+
+	// If a specific commit (Head) is provided, check it out
+	if !repoConfig.Head.IsZero() {
+		worktree, err := repoClone.Worktree()
+		if err != nil {
+			return plumbing.ZeroHash, err
+		}
+
+		// Attempt to resolve the hash (it may not be available yet)
+		_, err = repoClone.CommitObject(repoConfig.Head)
+		if err != nil {
+			// Commit not present: fetch the specific commit explicitly
+			err = repoClone.Fetch(&git.FetchOptions{
+				Depth:    1,
+				Progress: nil,
+				RefSpecs: []config.RefSpec{
+					config.RefSpec(fmt.Sprintf("+%s:refs/temp/%s", repoConfig.Head.String(), repoConfig.Head.String())),
+				},
+			})
+			if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+				return plumbing.ZeroHash, fmt.Errorf("failed to fetch specific commit: %w", err)
+			}
+		}
+
+		err = worktree.Checkout(&git.CheckoutOptions{
+			Hash: repoConfig.Head,
+		})
+		if err != nil {
+			return plumbing.ZeroHash, err
+		}
+	}
+
 	head, err := repoClone.Head()
 	if err != nil {
 		return plumbing.ZeroHash, err
